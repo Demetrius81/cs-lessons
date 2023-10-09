daae7092b4964107bc96bbd674b610de6a0efb29
Demetrius81 <shizimaniak@gmail.com>
2023-10-09 22:22:37 +0300

    seminar is done

--- FirstLessons/ConsoleCalculator.Tests/ConsoleCalculatorTests.cs
+++ FirstLessons/ConsoleCalculator.Tests/ConsoleCalculatorTests.cs
@@ -10,7 +10,4 @@ public class Tests
        _calculator = new Calculator();
     }
 
-    //[TestCase(new string[] { "2", "+", "3", "*", "5", "/", "5", "-", "4" }, ExpectedResult = true)]
-    //[TestCase(new string[] { "2", "r", "3", "*", "5", "/", "5", "-", "4" }, ExpectedResult = false)]
-    //public bool ValidateArgs_CorrectExpressions_ExpectTrue_Test1(string[] str) => _calculator.ValidateArgs(str);
 }
