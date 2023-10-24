a9e5f703d3624b957078f2b82c445bc546a7a3aa
Demetrius81 <shizimaniak@gmail.com>
2023-10-24 12:45:20 +0300

    hw in progress

---
+++ FirstLessons/Lesson6/HomeWork/FindNumbers.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6;
+internal class FindNumbers
+{
+    public static (int firstNumber, int secondNumber) FindTwoNumbersLoop(List<int> numbersList, int target)
+    {
+        for (int i = 0; i < numbersList.Count; i++)
+        {
+            for (int j = i + 1; j < numbersList.Count; j++)
+            {
+                if (numbersList[i] + numbersList[j] == target)
+                {
+                    return (numbersList[i], numbersList[j]);
+                }
+            }
+        }
+
+        return (0, 0);
+    }
+
+    public static (int firstNumber, int secondNumber) FindTwoNumbersHashSet(List<int> numbersList, int target)
+    {
+        HashSet<int> set = new();
+
+        foreach (var num in numbersList)
+        {
+            int firstNum = target - num;
+
+            if (set.Contains(firstNum))
+            {
+                return (firstNum, num);
+            }
+            else
+            {
+                set.Add(num);
+            }
+        }
+
+        return (0, 0);
+    }
+
+    public static (int firstNumber, int secondNumber, int thirdNumber) FindThreeNumbersLoop(List<int> numbersList, int target)
+    {
+        int iter = 0;
+
+        for (int i = 0; i < numbersList.Count; i++)
+        {
+            for (int j = i + 1; j < numbersList.Count; j++)
+            {
+                for (int k = j + 1; k < numbersList.Count; k++)
+                {
+                    iter++;
+
+                    if (numbersList[i] + numbersList[j] + numbersList[k] == target)
+                    {
+                        Console.WriteLine("Number of iterations = " + iter);
+                        return (numbersList[i], numbersList[j], numbersList[k]);
+                    }
+                }
+
+            }
+        }
+
+        return (0, 0, 0);
+    }
+
+    public static (int firstNumber, int secondNumber, int thirdNumber) FindThreeNumbersHashSet(List<int> numbersList, int target)
+    {
+        int iter = 0;
+        HashSet<int> set = new();
+
+        for (int i = 1; i < numbersList.Count; i++)
+        {
+            for (int j = i + 1; j < numbersList.Count; j++)
+            {
+                iter++;
+                int firstNum = target - numbersList[i] - numbersList[j];
+
+                if (set.Contains(firstNum))
+                {
+                    Console.WriteLine("Number of iterations = " + iter);
+                    return (firstNum, numbersList[i], numbersList[i]);
+                }
+                else
+                {
+                    set.Add(numbersList[i]);
+                }
+            }
+
+            set.Add(numbersList[i]);
+        }
+
+        return (0, 0, 0);
+    }
+
+    public static (int firstNumber, int secondNumber, int thirdNumber) FindThreeNumbersFromChatGPT(List<int> arr, int targetSum)
+    {
+        int iter = 0;
+        // Сортируем массив
+        arr.Sort();
+
+        // Перебираем первое число
+        for (int i = 0; i < arr.Count - 2; i++)
+        {
+            // Инициализируем указатели для второго и третьего чисел
+            int left = i + 1;
+            int right = arr.Count - 1;
+
+            while (left < right)
+            {
+                iter++;
+                int currentSum = arr[i] + arr[left] + arr[right];
+
+                if (currentSum == targetSum)
+                {
+                    // Найдены три числа сумма которых равна заданному числу
+                    Console.WriteLine("Number of iterations = " + iter);
+                    return (arr[i], arr[left], arr[right]);
+
+                }
+                else if (currentSum < targetSum)
+                {
+                    // Увеличиваем сумму, двигая указатель left вправо
+                    left++;
+                }
+                else
+                {
+                    // Уменьшаем сумму, двигая указатель right влево
+                    right--;
+                }
+            }
+        }
+
+        return (0, 0, 0);
+    }
+}
--- FirstLessons/Lesson6/Program.cs
+++ FirstLessons/Lesson6/Program.cs
@@ -4,7 +4,17 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Tasks tasks = new Tasks();
-        tasks.Task8();
+        List<int> ints = new List<int> { 1, 2, 3, 14, 5, 46, 7, 8, 9, 10, 4, 18, 6, };
+        int target = 78;
+
+        var result = FindNumbers.FindThreeNumbersHashSet(ints, target);
+        Console.WriteLine($"{result.firstNumber} + {result.secondNumber} + {result.thirdNumber} = {target}");
+
+        var result2 = FindNumbers.FindThreeNumbersFromChatGPT(ints, target);
+        Console.WriteLine($"{result2.firstNumber} + {result2.secondNumber} + {result2.thirdNumber} = {target}");
+
+        var result3 = FindNumbers.FindThreeNumbersLoop(ints, target);
+        Console.WriteLine($"{result3.firstNumber} + {result3.secondNumber} + {result3.thirdNumber} = {target}");
+
     }
 }
