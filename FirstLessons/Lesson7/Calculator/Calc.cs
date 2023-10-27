84ac23a4beee5743e114887428c708973c10d116
Demetrius81 <shizimaniak@gmail.com>
2023-10-27 17:29:20 +0300

    seminar is done

---
+++ FirstLessons/Lesson7/Calculator/Calc.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7;
+internal class Calc : ICalc
+{
+    private Stack<double> _lastStack;
+
+    internal Calc()
+    {
+        Result = 0;
+        _lastStack = new Stack<double>();
+    }
+
+    public event EventHandler<EventArgs> CalcEventHandler;
+
+    public double Result { get; private set; }
+
+    public void Sum(double x)
+    {
+        _lastStack.Push(Result);
+        Result += x;
+        PrintResult();
+    }
+
+    public void Sub(double x)
+    {
+        _lastStack.Push(Result);
+        Result -= x;
+        PrintResult();
+    }
+
+    public void Div(double x)
+    {
+        if (x == 0)
+        {
+            throw new ArithmeticException("Divide by zero");
+        }
+
+        _lastStack.Push(Result);
+        Result /= x;
+        PrintResult();
+    }
+
+    public void Mult(double x)
+    {
+        _lastStack.Push(Result);
+        Result *= x;
+        PrintResult();
+    }
+
+    public void CancelLast()
+    {
+        if (_lastStack.TryPop(out double x))
+        {
+            Result = x;
+            PrintResult();
+        }
+    }
+
+    private void PrintResult()
+    {
+        CalcEventHandler?.Invoke(this, new EventArgs());
+    }
+
+}
---
+++ FirstLessons/Lesson7/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson7;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Tasks task = new();
+        task.Task4();
+    }
+}
